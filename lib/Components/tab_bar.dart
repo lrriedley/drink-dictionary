365294af13ae112d9ff9a4f61189a0f287d653cc
Logan <loganriedley2@gmail.com>
2023-09-17 17:15:53 -0400

    add all files

---
+++ lib/Components/tab_bar.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:drink_dictionary/screens/home_screen.dart';
+
+class NewTabBar extends StatelessWidget {
+  const NewTabBar({
+    super.key,
+  });
+
+  final Color color1 = const Color(0xFF16191B);
+  final Color color2 = const Color(0xFF283337);
+  final Color color3 = const Color(0xFF16191B);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+      ),
+      child: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [color1, color2, color3],
+          ),
+        ),
+        child: const BottomTabBar(),
+      ),
+    );
+  }
+}
+
+class BottomTabBar extends StatelessWidget {
+  const BottomTabBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.transparent,
+      child: SizedBox(
+        height: 50.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.add_circle_rounded, color: Colors.white),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.home_filled, color: Colors.white),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => HomeScreen(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.local_bar, color: Colors.white),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.favorite, color: Colors.white),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
