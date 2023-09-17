2b3e800504f564ac34c19178349641d8d7b4d950
Logan <loganriedley2@gmail.com>
2023-09-17 17:16:28 -0400

    add files

---
+++ lib/screens/spirits_screen.dart
@@ -0,0 +1,184 @@
+import 'package:drink_dictionary/components/tab_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:drink_dictionary/drink_database.dart';
+import 'package:drink_dictionary/components/category_button.dart';
+import 'package:drink_dictionary/components/drink_card.dart';
+
+class SpiritsScreen extends StatefulWidget {
+  static const String id = 'spirits_screen';
+  @override
+  _SpiritsScreenState createState() => _SpiritsScreenState();
+}
+
+class _SpiritsScreenState extends State<SpiritsScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color1 = const Color(0xFF16191B);
+    final Color color2 = const Color(0xFF283337);
+    final Color color3 = const Color(0xFF16191B);
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.black.withOpacity(0.7),
+            title: Text(
+              'SPIRITS',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 32, // text size
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 40.0, // specify the height to make it smaller
+                    padding: const EdgeInsets.fromLTRB(8.0, 0, 0, 0),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [color1, color2, color3],
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                      ),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search for your favorite spirits',
+                              hintStyle: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(right: 8),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      CategoryButton(
+                        color1: color1,
+                        color2: color2,
+                        color3: color3,
+                        categoryName: 'Spirits',
+                      ),
+                      CategoryButton(
+                        color1: color1,
+                        color2: color2,
+                        color3: color3,
+                        categoryName: 'Cocktails',
+                      ),
+                      CategoryButton(
+                          color1: color1,
+                          color2: color2,
+                          color3: color3,
+                          categoryName: 'Beer'),
+                      CategoryButton(
+                          color1: color1,
+                          color2: color2,
+                          color3: color3,
+                          categoryName: 'Wine'),
+                      CategoryButton(
+                          color1: color1,
+                          color2: color2,
+                          color3: color3,
+                          categoryName: 'Seltzer'),
+                      CategoryButton(
+                          color1: color1,
+                          color2: color2,
+                          color3: color3,
+                          categoryName: 'Mixers'),
+                      CategoryButton(
+                          color1: color1,
+                          color2: color2,
+                          color3: color3,
+                          categoryName: 'Other')
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, .5, .5, .5),
+                  child: Text(
+                    'Trending',
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var drink in drinks)
+                        DrinkCard(
+                          drinkImage: drink['drinkImage'],
+                          drinkName: drink['drinkName'],
+                          drinkDescription: drink['drinkDescription'],
+                        ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, .5, .5, .5),
+                  child: Text(
+                    'Bourbon',
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var bourbonDrink in bourbon)
+                        DrinkCard(
+                          drinkImage: bourbonDrink['drinkImage'],
+                          drinkName: bourbonDrink['drinkName'],
+                          drinkDescription: bourbonDrink['drinkDescription'],
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: const NewTabBar(),
+        ),
+      ),
+    );
+  }
+}
