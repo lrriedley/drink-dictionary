ad2edc81704f8fb5cbd7c7820779ad6e16a1fb78
Logan <loganriedley2@gmail.com>
2023-11-03 18:57:47 -0400

    feat/modify components

---
+++ lib/Components/alphabet_scroll.dart
@@ -0,0 +1,63 @@
+import 'package:azlistview/azlistview.dart';
+import 'package:flutter/material.dart';
+import 'package:drink_dictionary/drinks.dart';
+import 'package:drink_dictionary/Components/drink_list.dart';
+
+class AZItem extends ISuspensionBean {
+  final String title;
+  final String tag;
+
+  AZItem({
+    required this.title,
+    required this.tag,
+  });
+  @override
+  String getSuspensionTag() => tag;
+}
+
+class AlphabetScrollWidget extends StatefulWidget {
+  final List<Drink> items;
+
+  const AlphabetScrollWidget({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
+
+  @override
+  State<AlphabetScrollWidget> createState() => _AlphabetScrollWidgetState();
+}
+
+class _AlphabetScrollWidgetState extends State<AlphabetScrollWidget> {
+  late List<AZItem> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initList(widget.items);
+  }
+
+  void initList(List<Drink> drinks) {
+    items = drinks
+        .map((drink) => AZItem(title: drink.drinkName, tag: drink.drinkName[0]))
+        .toList();
+  }
+
+  Widget buildList(Drink drink) {
+    return DrinkList(
+      drinkImage: drink.drinkImage,
+      drinkName: drink.drinkName,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AzListView(
+        padding: const EdgeInsets.all(16),
+        data: items,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final drink = widget.items[index];
+          return buildList(drink);
+        });
+  }
+}
---
+++ lib/Components/drink_list.dart
@@ -0,0 +1,85 @@
+import 'package:drink_dictionary/screens/drink_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DrinkList extends StatelessWidget {
+  final AssetImage drinkImage;
+  final String drinkName;
+  final String? drinkDescription;
+
+  const DrinkList({
+    Key? key,
+    required this.drinkImage,
+    required this.drinkName,
+    this.drinkDescription,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DrinkScreen(
+                drinkImage: drinkImage,
+                drinkName: drinkName,
+                drinkDescription: drinkDescription ?? ''),
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(5),
+                height: 106,
+                width: 97,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Image(
+                  image: drinkImage,
+                  width: 10,
+                  height: 10,
+                  alignment: Alignment.topCenter,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  drinkName,
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 1,
+            constraints: const BoxConstraints(
+              maxWidth: 350,
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.white, Colors.transparent],
+                begin: Alignment.center,
+                end: Alignment.centerRight,
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
